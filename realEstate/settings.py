e5bdd656746c0ebe65f1502c8074bb26241f302d
amirsallah <amir1381basketball@gmail.com>
2024-05-26 01:04:46 +0330

    feat: Add user app and it's models

--- realEstate/settings.py
+++ realEstate/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework_simplejwt',
+    
+    'products',
+    'users'
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+
+class Landlord(models.Model):
+    user = models.OneToOneField(User, verbose_name=_('user'), on_delete=models.CASCADE)
+    phone_number = models.CharField(_('phone_number'), unique=True, max_length=20)
+    email = models.EmailField(_('email'), unique=True)
+
+    class Meta(object):
+        db_table = _('landlord')
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
