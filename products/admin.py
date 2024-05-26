110ba7ebfa9256ec3ca2322c2a6c8639eb176dd9
amirsallah <amir1381basketball@gmail.com>
2024-05-26 23:21:41 +0330

    feat: replace user admin instead of custom user admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Estate, File
+
+
+@admin.register(Estate)
+class EstateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'city')
+    search_fields = ['title']
+    list_filter = ['city']
+
+
+class FileInlineAdmin(admin.TabularInline):
+    model = File
+    fields = ['id', 'title', 'file', 'order', 'is_active']
+    extra = 0
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from users.models import Landlord
+
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class LandlordInline(admin.StackedInline):
+    model = Landlord
+    can_delete = False
+    verbose_name_plural = 'landlords'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (LandlordInline,)
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
