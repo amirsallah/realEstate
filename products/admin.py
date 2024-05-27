728d8883947a65fc934c529718cd561c1686eacb
amirsallah <amir1381basketball@gmail.com>
2024-05-27 15:21:24 +0330

    fix: add jwt files to EstateAdmin

--- products/admin.py
+++ products/admin.py
@@ -3,14 +3,15 @@ from django.contrib import admin
 from products.models import Estate, File
 
 
+class FileInlineAdmin(admin.TabularInline):
+    model = File
+    fields = ['id', 'title', 'file', 'order', 'is_active']
+    extra = 0
+
+
 @admin.register(Estate)
 class EstateAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'city')
     search_fields = ['title']
     list_filter = ['city']
-
-
-class FileInlineAdmin(admin.TabularInline):
-    model = File
-    fields = ['id', 'title', 'file', 'order', 'is_active']
-    extra = 0
+    inlines = [FileInlineAdmin]
