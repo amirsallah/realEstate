473932098a080dffa387b2debb59c06341de9609
amirsallah <amir1381basketball@gmail.com>
2024-05-27 15:25:30 +0330

    fix: add name field to Landlord model

--- users/models.py
+++ users/models.py
@@ -4,11 +4,15 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Landlord(models.Model):
-    user = models.OneToOneField(User, verbose_name=_('user'), on_delete=models.CASCADE)
-    phone_number = models.CharField(_('phone_number'), unique=True, max_length=20)
+    user = models.OneToOneField(User, verbose_name=_('user'), on_delete=models.CASCADE, blank=False, null=False)
+    phone_number = models.CharField(_('phone_number'), unique=True, max_length=20, blank=False, null=False)
     email = models.EmailField(_('email'), unique=True)
+    name = models.CharField(_('name'), max_length=200, blank=False, null=False)
 
     class Meta(object):
         db_table = 'landlord'
         verbose_name = _('user')
         verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.user.username
